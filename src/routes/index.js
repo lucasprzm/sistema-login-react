b11e824b15bba9bdc8e35b6c91ae4caf4842ab34
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-02-25 12:16:21 -0300

    ajuste rotas login

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,36 +2,58 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import Dashboard from "../pages/Dashboard";
-// import Route from "./Route";
+
 export default function Rotas() {
   return (
     <Routes>
-      <Route path="/" element={<SignIn />} />
-      <Route path="/register" element={<SignUp />} />
+      <Route
+        path="/"
+        element={
+          <NaoLogado>
+            <SignIn />
+          </NaoLogado>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <NaoLogado>
+            <SignUp />
+          </NaoLogado>
+        }
+      />
       <Route
         path="/dashboard"
         element={
-          <Autenticacao>
+          <Logado>
             <Dashboard />
-          </Autenticacao>
+          </Logado>
         }
       />
     </Routes>
   );
 }
-
-function Autenticacao({ children }) {
-  const loading = false;
-  const signed = true;
-
+const loading = false;
+const signed = false;
+function Logado({ children }) {
   if (loading) {
     return <div></div>;
   }
-
   if (!signed) {
     return <Navigate to="/" />;
   }
   if (signed) {
     return children;
+  }
+}
+function NaoLogado({ children }) {
+  if (loading) {
+    return <div></div>;
+  }
+  if (!signed) {
+    return children;
+  }
+  if (signed) {
+    return <Navigate to="/dashboard" />;
   }
 }

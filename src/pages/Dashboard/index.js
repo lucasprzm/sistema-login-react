cf751a77bb30ebdbb6d772f016ac20f58e852984
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-02-25 18:26:12 -0300

    sidebar adicionada ao dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,15 @@
+import { useContext } from "react";
+import Header from "../../components/Header";
+import { AuthContext } from "../../contexts/auth";
+
 function Dashboard() {
+  const { signOut } = useContext(AuthContext);
+
   return (
-    <div className="Dashboard">
+    <div>
+      <Header />
       <h1>Página do Dashboard</h1>
+      <button onClick={() => signOut()}>Fazer logout</button>
     </div>
   );
 }

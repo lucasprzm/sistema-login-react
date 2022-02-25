00b0ad18de53dbeba3108e00076a309bb79d0f08
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-02-25 15:21:27 -0300

    criação página cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,46 @@
+import { useState } from "react";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
+
 function SignUp() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("Clicou!");
+  }
+
   return (
-    <div className="SignUp">
-      <h1>Página de Registro</h1>
+    <div className="container-center">
+      <div className="login">
+        <div className="logo-area">
+          <img src={logo} alt="Logomarca do Sistema" />;
+        </div>
+        <form onSubmit={handleSubmit}>
+          <h1>Cadastro</h1>
+          <input
+            type="text"
+            placeholder="Seu Nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Cadastrar</button>
+        </form>
+        <Link to="/">Já tem uma conta? Entre!</Link>
+      </div>
     </div>
   );
 }

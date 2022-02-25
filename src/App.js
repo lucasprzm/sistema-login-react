783f23f2d9c14a6d3a7ea6fdf8c4193d2e4320bb
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-02-25 15:22:00 -0300

    criação context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import Rotas from "./routes";
+import AuthProvider from "./contexts/auth";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Rotas />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Rotas />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
---
+++ src/contexts/auth.js
@@ -0,0 +1,29 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext({});
+
+function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loadingAuth, setLoadingAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    function loadStorage() {
+      const storageUser = localStorage.getItem("SistemaUser");
+      if (storageUser) {
+        setUser(JSON.parse(storageUser));
+        setLoading(false);
+      }
+      setLoading(false);
+    }
+    loadStorage();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ signed: !!user, user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;

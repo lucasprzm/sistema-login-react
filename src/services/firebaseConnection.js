a8bd5119e16887674d09ce0171f64efe2b480c93
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-03-02 10:27:35 -0300

    adicionado firebase storage

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCe3dPxd0s5KfWmHijCDsoMmd8hSX_X864",

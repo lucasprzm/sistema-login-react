cabd9de40a1c27651be00b7db6bf3d76efd4c2c4
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-03-02 15:29:39 -0300

    lógica de criação de chamados adicionada

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -2,11 +2,87 @@ import Header from "../../components/Header";
 import Title from "../../components/Title";
 import { FiPlusCircle } from "react-icons/fi";
 import "./new.css";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../contexts/auth";
+import firebase from "../../services/firebaseConnection";
+import { toast } from "react-toastify";
 
 export default function New() {
-  function handleRegister(e) {
+  const [loadCustomers, setLoadCustomers] = useState(true);
+  const [customers, setCustomers] = useState([]);
+  const [customerSelected, setCustomerSelected] = useState(0);
+  const [assunto, setAssunto] = useState("Suporte");
+  const [status, setStatus] = useState("Aberto");
+  const [complemento, setComplemento] = useState("");
+
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    async function loadCustomers() {
+      await firebase
+        .firestore()
+        .collection("customers")
+        .get()
+        .then((snapshot) => {
+          let lista = [];
+          snapshot.forEach((doc) => {
+            lista.push({
+              id: doc.id,
+              nomeFantasia: doc.data().nomeFantasia,
+            });
+          });
+          if (lista.length === 0) {
+            console.log("Nenhuma Empresa Encontrada");
+            setCustomers([{ id: "1", nomeFantasia: "Freela" }]);
+            setLoadCustomers(false);
+            return;
+          }
+          setCustomers(lista);
+          setLoadCustomers(false);
+        })
+        .catch((error) => {
+          console.log("Deu algum erro!");
+          setLoadCustomers(false);
+          setCustomers([{ id: "1", nomeFantasia: "" }]);
+        });
+    }
+    loadCustomers();
+  }, []);
+
+  async function handleRegister(e) {
     e.preventDefault();
-    alert("teste");
+    await firebase
+      .firestore()
+      .collection("chamados")
+      .add({
+        created: new Date(),
+        cliente: customers[customerSelected].nomeFantasia,
+        clienteId: customers[customerSelected].id,
+        assunto: assunto,
+        status: status,
+        complemento: complemento,
+        userId: user.uid,
+      })
+      .then(() => {
+        toast.success("Chamado criado com sucesso!");
+        setComplemento("");
+        setCustomerSelected(0);
+      })
+      .catch((error) => {
+        toast.error("Ops! Erro ao registrar, tente mais tarde.");
+        console.log(error);
+      });
+  }
+  function handleChangeSelect(e) {
+    setAssunto(e.target.value);
+  }
+  function handleOptionChange(e) {
+    setStatus(e.target.value);
+  }
+  function handleChangeCustomers(e) {
+    // console.log("Index do Cliente Selecionado:", e.target.value);
+    // console.log("Cliente Selecionado", customers[e.target.value]);
+    setCustomerSelected(e.target.value);
   }
 
   return (
@@ -19,30 +95,62 @@ export default function New() {
         <div className="container">
           <form className="form-profile" onSubmit={handleRegister}>
             <label>Cliente</label>
-            <select>
-              <option key={1} value={1}>
-                Sujeito Programador
-              </option>
-            </select>
+            {loadCustomers ? (
+              <input
+                type="text"
+                disabled={true}
+                value="Carregando Clientes..."
+              />
+            ) : (
+              <select value={customerSelected} onChange={handleChangeCustomers}>
+                {customers.map((item, index) => {
+                  return (
+                    <option key={item.id} value={index}>
+                      {item.nomeFantasia}
+                    </option>
+                  );
+                })}
+              </select>
+            )}
             <label>Assunto</label>
-            <select>
+            <select value={assunto} onChange={handleChangeSelect}>
               <option value="Suporte">Suporte</option>
               <option value="Visita Técnica">Visita Técnica</option>
               <option value="Financeiro">Financeiro</option>
             </select>
             <label>Status</label>
             <div className="status">
-              <input type="radio" name="radio" value="Aberto" />
+              <input
+                type="radio"
+                name="radio"
+                value="Aberto"
+                onChange={handleOptionChange}
+                checked={status === "Aberto"}
+              />
               <span>Em Aberto</span>
-              <input type="radio" name="radio" value="Progresso" />
+              <input
+                type="radio"
+                name="radio"
+                value="Progresso"
+                onChange={handleOptionChange}
+                checked={status === "Progresso"}
+              />
               <span>Em Andamento</span>
-              <input type="radio" name="radio" value="Atendido" />
+              <input
+                type="radio"
+                name="radio"
+                value="Atendido"
+                onChange={handleOptionChange}
+                checked={status === "Atendido"}
+              />
               <span>Atendido</span>
             </div>
             <label>Complemento</label>
             <textarea
               type="text"
               placeholder="Descreva seu problema (opcional)."
+              value={complemento}
+              onChange={(e) => setComplemento(e.target.value)}
             />
             <button type="submit">Salvar</button>
           </form>
